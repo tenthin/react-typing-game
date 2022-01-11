cf8ef4c5991da80532209d494e38185166da8264
Tenzin <tenthinten@gmail.com>
2022-01-11 13:42:20 +0530

    making the active letter bold for better visualization

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ windows bitcoin dogecoin mars tesla space editors mars earth heater water air ja
 react boat blanket`.split(' ').sort(() => Math.random() > 0.5 ? 1 : -1)
 
 function App() {
+
   const [userInput, setUserInput] = useState('')
   
-  const cloud = useRef(getCloud)
+  const cloud = useRef(getCloud())
+
+  const [activeWrodIndex, setActiveWrodIndex] = useState(0)
 
  
   return (
 
     <div>
       <h1>Typing Test</h1>
-      <p>{cloud.current.toString()}</p>
+      <p>{cloud.current.map((word, index) => {
+
+        if(index == activeWrodIndex) {
+          return <b>{word} </b>
+        }
+
+        return <span>{word} </span> 
+      })}</p>
 
       <input 
       type="text" 
