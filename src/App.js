26709330af347675cee5aa1b7df616afe253213d
Tenzin <tenthinten@gmail.com>
2022-01-11 17:44:07 +0530

    change the word after completing each word

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './App.css';
 
 const getCloud = () => `codeamn hello eleonmusk react playground yo by amazon apple macbook
 windows bitcoin dogecoin mars tesla space editors mars earth heater water air javascripts
-react boat blanket`.split(' ').sort(() => Math.random() > 0.5 ? 1 : -1)
+react boat blanket`.split(' ')
+// .sort(() => Math.random() > 0.5 ? 1 : -1)
 
 function App() {
 
   const [userInput, setUserInput] = useState('')
-  
   const cloud = useRef(getCloud())
 
   const [activeWrodIndex, setActiveWrodIndex] = useState(0)
 
- 
+  function processInput(value) {
+    if(value.endsWith(' ')) {
+      // that means the user has finised this word
+      setActiveWrodIndex(index => index + 1)
+      setUserInput(' ')
+    }else{
+      setUserInput(value)
+    }
+  }
   return (
 
     <div>
@@ -31,7 +39,7 @@ function App() {
       <input 
       type="text" 
       value={userInput} 
-      onChange={(e) => setUserInput(e.target.value)}
+      onChange={(e) => processInput(e.target.value)}
       />
 
     </div>
