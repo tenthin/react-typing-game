985898d342f2da4e00fd461a0d1280b347fb6d39
Tenzin <tenthinten@gmail.com>
2022-01-11 13:32:57 +0530

    words to type

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import './App.css';
+
+
+const getCloud = () => `codeamn hello eleonmusk react playground yo by amazon apple macbook
+windows bitcoin dogecoin mars tesla space editors mars earth heater water air javascripts
+react boat blanket`.split(' ').sort(() => Math.random() > 0.5 ? 1 : -1)
 
 function App() {
   const [userInput, setUserInput] = useState('')
+  
+  const cloud = useRef(getCloud)
 
+ 
   return (
 
     <div>
       <h1>Typing Test</h1>
-      <p>{userInput}</p>
+      <p>{cloud.current.toString()}</p>
 
       <input 
       type="text" 
