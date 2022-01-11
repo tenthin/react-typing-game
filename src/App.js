bfd6c6723e831bf64330542f774b318e68f42806
Tenzin <tenthinten@gmail.com>
2022-01-11 11:38:44 +0530

    form space to type in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1>Typing Test</h1>
+      <p>{userInput}</p>
+
+      <input 
+      type="text" 
+      value={userInput} 
+      onChange={(e) => setUserInput(e.target.value)}
+      />
+
     </div>
   );
 }
